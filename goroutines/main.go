0820893ef2890915feb03de99f23627650492f08
GaganBansal <bansalgagan2004@gmail.com>
2026-08-08 11:28:05 +0530

    [Learn & Code] : Goroutines

---
+++ goroutines/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func task(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Task", id, "is running")
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		go task(i, &wg)
+	}
+
+	wg.Wait()
+}
